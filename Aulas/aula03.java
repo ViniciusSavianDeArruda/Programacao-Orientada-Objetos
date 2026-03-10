fd917113e6820b0e2a983d08c4af5c3520b5e93e
Vinicius Arruda <arrudavinicius283@gmail.com>
2026-03-10 15:59:36 -0300

    Enable user input for vectors and matrices in aula03
    Added user input functionality for both vectors and matrices in aula03, allowing dynamic element entry.

--- Aulas/aula03.java
+++ Aulas/aula03.java
@@ -1,4 +1,53 @@
+package pkg;
+import java.util.Scanner;
 
-public class aula03 {
+public class Vetores {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int[] vetor = new int[5];
+//		vetor[0] = 10;
+//		vetor[1] = 15;
+//		vetor[2] = 20;
+//		vetor[3] = 25;
+//		vetor[4] = 30;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("Digite o elemento " + i + ":");
+			vetor[i] = teclado.nextInt();
+		}
+	
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println("Elemento " + i + "= " + vetor[i]);
+		}
+	}
+
+}
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int[][] matriz = new int[2][3];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Digite o elemento [" + i + "][" + j + "]:");
+                matriz[i][j] = teclado.nextInt();
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + "\t"); // isso é igual a system.out.print("Elemento ["+i+"]["+j+"]: " + matriz[i][j]); ");
+                
+            }
+            System.out.println();
+        }
+
+        teclado.close();
+    }
 
 }
